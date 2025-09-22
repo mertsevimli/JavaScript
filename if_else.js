5a9df5e716ced55ebaff923410f7ee79f403a8a0
mertsevimli <mert234tr@gmail.com>
2025-09-22 14:16:48 +0300

    Add initial user authentication logic with username and password validation

---
+++ if_else.js
@@ -0,0 +1,19 @@
+let username = "adminmert";
+let password = "1234";
+
+if (username == "adminmert") {
+    if (password == "12345") {
+        console.log("uygulamaya giriş yapıldı. Hoşgeldiniz " + username);
+    } else {
+        console.log("parola yanlış.");
+    }
+}
+else {
+    console.log("kullanıcı adı yanlış.");
+}
+
+// if (!isLoggedin) {
+//     console.log("merhaba");
+// }
+
+
