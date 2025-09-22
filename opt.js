1119b9af693768d476b3d976edd50bd9b792a5a0
mertsevimli <mert234tr@gmail.com>
2025-09-22 13:59:15 +0300

    Refactor arithmetic, assignment, and comparison operations examples for clarity and consistency

--- opt.js
+++ opt.js
@@ -1,47 +1,33 @@
 let sonuc;
-let a = 10;
-let b = 20;
-let c = 30;
+let a = 15, b = 15, c = 32;
 
+// 1- Aritmetik Operatörler
+sonuc = a + b;
+sonuc = a - b;
+sonuc = a * b;
+sonuc = a / b;
+sonuc = c % a;
+// sonuc = a++;    // a + 1
+// sonuc = ++a;    // a + 1
+// sonuc = --a;
+// sonuc = a--;
 
-//* Aritmatik işlemler
-sonuc = a + b; // toplama
-sonuc = a - b; // çıkarma
-sonuc = a * b; // çarpma
-sonuc = a / b; // bölme
-sonuc = c % a; // mod alma
+// 2- Atama Operatörleri
+sonuc = a;
+sonuc += a // sonuc = sonuc + a 
+sonuc -= a // sonuc = sonuc - a 
+sonuc *= a // sonuc = sonuc * a 
+sonuc /= a // sonuc = sonuc / a 
+sonuc %= a // sonuc = sonuc % a 
 
-//* Atama işlemleri
-sonuc = a; // atama
-sonuc += a; // sonuc = sonuc + a;
-sonuc -= a; // sonuc = sonuc - a;
-sonuc *= a; // sonuc = sonuc * a;
-sonuc /= a; // sonuc = sonuc / a;
-sonuc %= a; // sonuc = sonuc % a;
+// 3- Karşılaştırma Operatörleri
+sonuc = (a == b)
+sonuc = (a != b)
+sonuc = (3 === "3") // değer tip
+sonuc = (a > b)
+sonuc = (a < b)
+sonuc = (a <= b)
 
-//Karşılaştırma işlemleri
-sonuc = (a == b); // eşit mi
-sonuc = (a != b); // eşit değil mi
-sonuc = (a === b); // hem değer hem tür olarak eşit mi
-sonuc = (a !== b); // hem değer hem tür olarak eşit değil mi
-sonuc = (a > b); // büyük mü
-sonuc = (a < b); // küçük mü
-sonuc = (a >= b); // büyük eşit mi
-sonuc = (a <= b); // küçük eşit mi
-
-//* Mantıksal işlemler
-sonuc = a < b; // küçüktür
-sonuc = a > b; // büyüktür
-sonuc = a <= b; // küçük eşittir
-sonuc = a >= b; // büyük eşittir
-sonuc = a == b; // eşittir
-sonuc = a != b; // eşit değildir
-
-//* Artırma ve Azaltma işlemleri
-sonuc = ++a; // a'nın değeri 1 artar ve sonra atama yapılır
-sonuc = --a; // a'nın değeri 1 azalır ve sonra atama yapılır
-sonuc = a++; // atama yapılır sonra a'nın değeri 1 artar
-sonuc = a--; // atama yapılır sonra a'nın değeri 1 azalır   
+// 4- Mantıksal Operatörler
 
 console.log(sonuc);
-console.log(a);
